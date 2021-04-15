8661bbff79d8f7a8bd149e99dd2bb60d8e8eeee3
mattyg915 <matt.gercz@icloud.com>
2021-04-15 18:37:47 -0500

    fix convolution indices

--- image_sources/convolution.cpp
+++ image_sources/convolution.cpp
@@ -1,10 +1,10 @@
 #include "../image_headers/convolution.h"
 #include <vector>
 
-int calcFx(const uint8_t* image, size_t i, size_t j, std::size_t width, std::size_t height) {
+float calcFx(const uint8_t* image, size_t i, size_t j, std::size_t width, std::size_t height) {
     if (0 <= i && i < width && 0 <= j && j < height)
     {
-        return image[i * width + j];
+        return image[j * width + i];
     }
     else if ((0 <= i && i < width) || (0 <= j && j < height))
     {
@@ -18,17 +18,17 @@ int calcFx(const uint8_t* image, size_t i, size_t j, std::size_t width, std::siz
 
 void convolve(uint8_t* image, uint8_t* output, size_t width, size_t height, const float *mask, size_t m)
 {
-    for (size_t x = 0; x < width; x++)
+    for (size_t y = 0; y < height; y++)
     {
-        for (size_t y = 0; y < height; y++)
+        for (size_t x = 0; x < width; x++)
         {
-            output[x * width + y] = 0;
+            output[y * width + x] = 0;
             for (size_t i = 0; i < m; i++)
             {
                 for (size_t j = 0; j < m; j++)
                 {
                     float result = calcFx(image, x + i - m / 2, y + j - m / 2, width, height);
-                    output[x * width + y] += mask[i * m + j] * result;
+                    output[y * width + x] += mask[i * m + j] * result;
                 }
             }
         }
