929682d7748fd79f02cd588a54c85a82b3f74f94
mattyg915 <matt.gercz@icloud.com>
2021-05-01 22:24:16 -0500

    incorrect header decl

--- image_headers/image_utils.cuh
+++ image_headers/image_utils.cuh
@@ -2,6 +2,6 @@
 #define IMAGE_UTILS_CUH
 
 bool load_image(std::vector<unsigned char>& image, const char* filename, int& x, int&y, int& features, int force_features);
-__global__ void rgb_to_greyscale(int width, int height, std::vector<unsigned char>& orig_image, unsigned char* output);
+void rgb_to_greyscale(int width, int height, std::vector<unsigned char>& orig_image, unsigned char* output);
 
 #endif //IMAGE_UTILS_CUH
