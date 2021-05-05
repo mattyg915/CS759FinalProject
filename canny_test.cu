b6c77a654ba38b1cf7ef9b7dca4a0f115890d6c9
RyanHall517 <70591109+RyanHall517@users.noreply.github.com>
2021-05-04 22:24:56 -0500

    Parallel Canny test test file

---
+++ canny_test.cu
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include "image_headers/image_utils.h"
+#include "image_headers/convolution.h"
+#include "image_headers/canny.cuh"
+
+extern "C" {
+#define STB_IMAGE_IMPLEMENTATION
+#include "image_headers/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "image_headers/stbi_image_write.h"
+}
+
+int main(int argc, char* argv[])
+{
+    #define CHANNEL_NUM 3
+
+    using std::cout;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration;
+
+    // Must have exactly 1 command line argument
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./main filename" << std::endl;
+        exit(1);
+    }
+
+    char* filename = argv[1];
+
+    int width, height, features;
+    std::vector<unsigned char> image;
+    bool image_loaded = load_image(image, filename, width, height, features, CHANNEL_NUM);
+
+    if (!image_loaded)
+    {
+        std::cout << "Error loading image\n";
+        exit(1);
+    }
+
+    cout << "Image width = " << width << std::endl;
+    cout << "Image height = " << height << std::endl;
+
+    auto* pixels = new unsigned char[width * height];
+    auto* canny_output = new unsigned char[width * height];
+    auto* I_x = new uint8_t[width * height];
+    auto* I_y = new uint8_t[width * height];
+    float* gradient = new float[width * height];
+    float* theta = new float[width * height];
+
+    // copy data to the device
+    unsigned char *dpixels, *dcanny_output;
+    auto *dI_x, *dI_y;
+    float *dgradient, *dtheta;
+
+    cudaMalloc((void **)&dpixels, size * sizeof(unsigned char));
+    cudaMalloc((void **)&dcanny_output, size * sizeof(unsigned char));
+    
+    cudaMalloc((void **)&dI_x, size * sizeof(auto));
+    cudaMalloc((void **)&dI_y, size * sizeof(auto));
+    
+    cudaMalloc((void **)&dgradient, size * sizeof(float));
+    cudaMalloc((void **)&dtheta, size * sizeof(float));
+
+    cudaMemcpy(dpixels, pixels, size * sizeof(unsigned char), cudaMemcpyHostToDevice);
+
+    rgb_to_greyscale(width, height, image, pixels);
+
+    // generate timing variables
+	cudaEvent_t startEvent, stopEvent;
+	cudaEventCreate(&startEvent);
+	cudaEventCreate(&stopEvent);
+
+	// timing
+	cudaEventRecord(startEvent, 0);
+
+	canny(pixels, canny_output, theta, gradient, I_x, I_y, width, height);
+
+	// timing
+	cudaEventRecord(stopEvent, 0);
+	cudaEventSynchronize(stopEvent);
+	float elapsedTime;
+	cudaEventElapsedTime(&elapsedTime, startEvent, stopEvent);
+	std::cout << elapsedTime << "\n";
+
+    // copy back
+    cudaMemcpy(canny_output, dcanny_output, size * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+
+    stbi_write_jpg("canny.jpg", width, height, 1, canny_output, 100);
+
+    return 0;
+}
