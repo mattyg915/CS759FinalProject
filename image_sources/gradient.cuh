78a7251dd266ba50ac91f24e8c889eb6f716aecf
RyanHall517 <70591109+RyanHall517@users.noreply.github.com>
2021-05-06 15:20:06 -0500

    get rid of extraneous header file

--- image_sources/gradient.cuh
+++
@@ -1,9 +0,0 @@
-#ifndef GRADIENT_CUH
-#define GRADIENT_CUH
-#include <vector>
-#include <cstddef>
-
-__global__ void gradient_kernel(uint8_t* I_x, uint8_t* I_y, float* output, size_t width, size_t height);
-__global__ void angle_kernel(uint8_t* I_x, uint8_t* I_y, float* output, size_t width, size_t height);
-
-#endif
