d1050eddbf94385446757d9244ac69be7e14b06b
RyanHall517 <70591109+RyanHall517@users.noreply.github.com>
2021-05-06 15:19:38 -0500

    get rid of extraneous header file

--- image_sources/canny.cuh
+++
@@ -1,8 +0,0 @@
-#ifndef CANNY_CUH
-#define CANNY_CUH
-#include <vector>
-#include <cstddef>
-
-void canny(uint8_t* image, uint8_t* output, float* theta, float* gradient, uint8_t* I_x, uint8_t* I_y, size_t width, size_t height);
-
-#endif
