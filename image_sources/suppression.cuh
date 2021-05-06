ac298c91ff12fc5f2832d9c434fa283969f0e5ae
RyanHall517 <70591109+RyanHall517@users.noreply.github.com>
2021-05-06 15:20:14 -0500

    get rid of extraneous header file

--- image_sources/suppression.cuh
+++
@@ -1,8 +0,0 @@
-#ifndef SUPPRESSION_CUH
-#define SUPPRESSION_CUH
-#include <vector>
-#include <cstddef>
-
-__global__ void suppression_kernel(uint8_t* image, uint8_t* output, size_t width, size_t height, const float *gradient, const float *theta);
-
-#endif
